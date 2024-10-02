6e5e60ba2b9682f720ac6c2ee7224562f194164c
Nabil GAYL <gaylnabil@gmail.com>
2024-10-02 17:50:00 +0100

    adding create, update and delete books api

--- api.py
+++ api.py
@@ -22,3 +22,34 @@ async def retrieve_book(book_id: UUID):
 async def get_books():
     return books
 
+
+@app.post('/books', response_model=Book)
+async def create_book(bk: Book):
+    bk.id = uuid4()
+    books.append(bk)
+    return bk
+
+@app.put('/books/{book_id}', response_model_by_alias=Book)
+async def update_book(book_id: UUID, book_updated: Book):
+    
+    for idx, bk in enumerate(books):
+        if bk.id == book_id:
+            book_updated = bk.copy(update=book_updated.dict(exclude_unset=True))
+            books[idx] = book_updated
+            return book_updated
+
+    return HTTPException(status_code=404, detail=f'the book is not found')
+
+@app.delete('/books/{id}')
+async def delete_book(book_id: UUID):
+    
+    for bk in books:
+        if bk.id == book_id:
+            books.remove(bk)
+            return books
+            
+    return HTTPException(status_code=404, detail=f'the book is not found')
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
